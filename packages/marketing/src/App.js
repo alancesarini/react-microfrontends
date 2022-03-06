dd0cb1f49a582330457ffa07e4c850bb98eec9fd
Alan Cesarini <alan.cesarini@innoveo.com>
2022-03-06 11:35:48 +0100

    added auth

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import {
-	StylesProvider,
-	createGenerateClassName,
+  StylesProvider,
+  createGenerateClassName,
 } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const generateClassname = createGenerateClassName({
-	productionPrefix: 'ma',
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
 });
 
-export default () => {
-	return (
-		<div>
-			<StylesProvider generateClassName={generateClassname}>
-				<BrowserRouter>
-					<Switch>
-						<Route exact path='/pricing' component={Pricing} />
-						<Route path='/' component={Landing} />
-					</Switch>
-				</BrowserRouter>
-			</StylesProvider>
-		</div>
-	);
+export default ({ history }) => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  );
 };
